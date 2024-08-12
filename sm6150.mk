fea7cebe6172e705e15fb8d6bb17fd7ad98f497e
K R I S H N A <krishnakripa34567@gmail.com>
2024-08-12 12:21:37 +0000

    sm6150-common: Adapt for Project Elixir
    Signed-off-by: K R I S H N A <krishnakripa34567@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -302,7 +302,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/custom/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi
 
